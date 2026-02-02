34284660e20b4def912ad560fa5e4f1139e70ccc
peachfruittea <chocotin1624@gmail.com>
2026-02-02 16:44:13 +0800

    調整base專案名稱

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/InnerSoul/' : '/',
+  base: process.env.NODE_ENV === "production" ? "/testInnerSoul/" : "/",
   plugins: [react()],
-})
+});
